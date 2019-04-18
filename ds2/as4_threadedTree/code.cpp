95cf93f89f104e5754549b48e5a48b6fc9647189
Neel Khalade <neel99khalade@gmail.com>
2019-04-18 14:46:47 +0530

    BTT

--- ds2/as4_threadedTree/code.cpp
+++ ds2/as4_threadedTree/code.cpp
@@ -1,99 +1,159 @@
 #include<iostream>
 using namespace std;
 
-class tTreeNode {
-  int data;
-  bool lbit, rbit;
-  tTreeNode *left, *right;
-  friend class ThreadedTree;
-};
-
-class ThreadedTree {
-  tTreeNode* head;
+class node
+{
 public:
-  ThreadedTree() {
-    head = new tTreeNode;
-    head->rbit = 1;
-    head->lbit = 0;
-    head->right = head->left = head;
-  }
-
-  void createThreadedTree() {
-    tTreeNode *root,*temp,*curr;
-    char side;
-    int more;
-
-    root = new tTreeNode;
-    cout<<"\n Enter root data";
-    cin>>root->data;
-    head->lbit = 1;
-    head->left = root;
-    root->left = root->right = head;
-    root->lbit = root->rbit = 0;
-
-    do {
-      temp = root;
-      bool flag = 1;
-      curr = new tTreeNode;
-      cout<<"\n Enter data- ";
-      cin>>curr->data;
-      curr->lbit = curr->rbit = 0;
-
-      while(flag) {
-        cout<<"Where u want to inset? l or r";
-        cin>>side;
-
-        if(side == 'l') {
-          if(temp->lbit == 0) {
-            curr->left = temp->left;
-            temp->lbit = 1;
-            temp->left = curr;
-            flag = 0;
-          }
-          temp = temp->left;
-        } else if(side == 'r') {
-          if(temp->rbit == 0) {
-            curr->right = temp->right;
-            temp->rbit = 1;
-            temp->right = curr;
-            flag = 0;
-          }
-          temp = temp->right;
-        }
-      }
-
-      cout<<"Want to insert more? 1.Yes 0.No";
-      cin>>more;
-    } while(more);
-  }
-
-  tTreeNode* insucc(tTreeNode *temp) {
-    tTreeNode *x = new tTreeNode;
-    if(temp == NULL) return NULL;
-
-    while(temp->lbit == 1) {
-      temp = temp->left;
-    }
-
-    return temp;
-  }
-
-  void inorderTraversal() {
-    tTreeNode *temp;
-    temp = head;
-
-    while(1) {
-      temp = insucc(temp);
-      if(temp == head) break;
-      cout<<temp->data;
-    }
-  }
+	node *left,*right;
+	int lbit,rbit,data;
+	friend class tbt;	
 };
 
+class tbt
+{
+node *head;
+public:
+	
+	tbt()
+	{
+		head->right=head;
+		head->lbit=0;
+		head->rbit=1;
+	}
 
-int main(void) {
-    ThreadedTree t;
-    t.createThreadedTree();
-    t.inorderTraversal();
-    return 0;
+	void create();
+	node* insuc(node *);
+	void inorder();
+
+
+};
+
+void tbt::create()
+{
+	node *root,*curr,*temp;
+	char side,yn;
+
+	root=new node;
+	
+	cout<<"enter the data for the root"<<endl;
+	cin>>root->data;
+
+	head->left=root;
+	head->lbit=1;
+
+	root->left=root->right=head;
+	root->lbit=root->rbit=0;
+
+	
+
+	do
+	{
+		int flag=0;
+		curr=new node;
+		temp=root;
+		cout<<"enter the data of node"<<endl;
+		cin>>curr->data;
+
+		while(flag==0)
+		{
+			cout<<"press l to add node to left and r to right of "<<temp->data;
+			cin>>side;
+
+			if(side=='l')
+			{
+				if(temp->lbit==0)
+				{
+					curr->left=temp->left;
+					temp->left=curr;
+					temp->lbit=1;
+
+					curr->lbit=curr->rbit=0;
+					curr->right=temp;
+					flag=1;
+						
+				}
+				temp=temp->left;
+			}
+		
+			if(side=='r')
+			{
+				if(temp->rbit==0)
+				{
+					curr->right=temp->right;
+					temp->right=curr;
+					temp->rbit=1;
+
+					curr->lbit=curr->rbit=0;
+					curr->left=temp;
+					flag=1;
+						
+				}
+				temp=temp->right;
+			}
+
+		}
+			cout<<"press y to continue and n to stop"<<endl;
+			cin>>yn;
+
+		}while(yn=='y');
+
 }
+
+
+node* tbt::insuc(node *temp)
+{
+	node *x;
+	x=new node;
+	x=temp->right;
+	if(temp->rbit==1)
+	
+		while(x->lbit==1)
+		
+			x=x->left;
+		
+	
+	return x;
+}
+
+
+void tbt::inorder()
+{
+	node *temp;
+	temp=new node;
+	temp=head;
+
+	while(1)
+	{
+		temp=insuc(temp);
+		if(temp==head)
+		break;
+		cout<<temp->data;
+	}
+}
+int main()
+{
+	tbt t;
+	t.create();
+	t.inorder();
+
+	return 0;
+}
+
+/*
+OUTPUT:
+
+enter the data for the root
+4        
+enter the data of node
+3
+press l to add node to left and r to right of 4l
+press y to continue and n to stop
+y
+enter the data of node
+6
+press l to add node to left and r to right of 4r
+press y to continue and n to stop
+n
+346
+
+*/
